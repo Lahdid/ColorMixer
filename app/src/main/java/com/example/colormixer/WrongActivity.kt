4a9987eddce859f006d03dd44aee88c6dbffdc8d
Ahmed Kazez <ahmedgazez@gmail.com>
2023-02-10 11:45:34 +0100

    first commit

---
+++ app/src/main/java/com/example/colormixer/WrongActivity.kt
@@ -0,0 +1,11 @@
+package com.example.colormixer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WrongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.wrong)
+    }
+}
